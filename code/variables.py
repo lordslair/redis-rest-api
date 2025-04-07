b36e198f247eb10ee74e363abb2c376dabfc5748
lordslair <5469390+lordslair@users.noreply.github.com>
2025-04-07 20:53:12 +0200

    fix: Removed variables

--- code/variables.py
+++ code/variables.py
@@ -10,11 +10,9 @@ env_vars = {
     "ACCESS_TOKEN": os.environ.get("ACCESS_TOKEN", None),
     "CODE_ENOTFOUND": int(os.environ.get('CODE_ENOTFOUND', 404)),
     "GUNICORN_CHDIR": os.environ.get("GUNICORN_CHDIR", '/code'),
-    "GUNICORN_HOST": os.environ.get("GUNICORN_HOST", "0.0.0.0"),
-    "GUNICORN_PORT": os.environ.get("GUNICORN_PORT", 5000),
     "GUNICORN_BIND": f'{os.environ.get("GUNICORN_HOST", "0.0.0.0")}:{os.environ.get("GUNICORN_PORT", 5000)}',  # noqa: E501
-    "GUNICORN_WORKERS": os.environ.get("GUNICORN_WORKERS", 1),
-    "GUNICORN_THREADS": os.environ.get("GUNICORN_THREADS", 2),
+    "GUNICORN_WORKERS": int(os.environ.get("GUNICORN_WORKERS", 1)),
+    "GUNICORN_THREADS": int(os.environ.get("GUNICORN_THREADS", 2)),
     "GUNICORN_RELOAD": os.environ.get("GUNICORN_RELOAD", True),
 }
 # Print the environment variables for debugging
